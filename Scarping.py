bec09f0ade0237fd6bd5866b50066885f61efdff
GregorD1A1 <dudnikgrv@gmail.com>
2021-09-25 19:53:47 +0200

    Program rozwinięty, pozwala pobierać do 400 obrazów
    By dopracować do idealu, zostaje parę drobnych popraw

--- Scarping.py
+++ Scarping.py
@@ -9,10 +9,13 @@ driver = webdriver.Firefox()
 search_key = 'spitfire'
 dataset_dir = 'dataset_dir'
 fullHD_height = 1080
+n_scrolls = 6
 
 search_window_xpath = "//input[@type='text']"
 accept_cookie_button_xpath = "//div[contains(text(),'Zgadzam')]"
-images_xpath = "//div[1]/div[1]/div[1]/span[1]/div[1]/div[1]/div[.]/a[1]/div[1]/img[1]"
+images_xpath = "//div[1]/div[1]/span[1]/div[1]/div[1]/div[.]/a[1]/div[1]/img[1]"
+
+
 fin_img_xpath = "//div[1]/div[1]/div[1]/span[1]/div[1]/div[1]/div[98]/a[1]/div[1]/img[1]"
 
 # program
@@ -33,14 +36,19 @@ search_window.send_keys(Keys.RETURN)
 # wyszukiwanie i pobieranie obrazów
 Wait(driver,30).until(ExpCon.presence_of_element_located((
     By.XPATH, images_xpath)))
-driver.execute_script(f"window.scrollTo(0, {str(2*fullHD_height)})")   # skrolowanie, by pojawiło się więcej obrazów
-Wait(driver,30).until(ExpCon.presence_of_element_located((
-    By.XPATH, fin_img_xpath)))
-images = driver.find_elements_by_xpath(images_xpath)
-print(len(images))
-for nr, image in enumerate(images):
-    with open(f'dataset_dir/{search_key}_{nr}.png', 'wb') as file:
-        file.write(image.screenshot_as_png)
+for _ in range(n_scrolls):
+    driver.execute_script(f"window.scrollTo(0, {str(2*fullHD_height)})")   # skrolowanie, by pojawiło się więcej obrazów
+
+    images = driver.find_elements_by_xpath(images_xpath)
+    print(len(images))
+    # tak właściwie to tylko zapisy z ostatniej iteracji się liczą, poprzednie są nadpisywane. Poprzednie iteracje
+    # jednak są potrzebne, by poskakać po obrazach i one się utrwaliły do tej ostatniej
+    for nr, image in enumerate(images):
+        with open(f'dataset_dir/{search_key}_{nr}.png', 'wb') as file:
+            file.write(image.screenshot_as_png)
+
+    #if n_scroll == 5:
+        #pokaż więcej wyników.click()
 
 # zamykanie przeglądarki
 driver.close()
